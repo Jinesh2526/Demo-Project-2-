ddf1fc66fc4b0d6e574e4b10fb7e5fe34a0bcf6c
Jinesh2526 <141109330+Jinesh2526@users.noreply.github.com>
2023-08-13 15:16:43 +0530

    4 files modified, 3 added

---
+++ src/samsungapp/samsung/samsung-box-inside/samsung-box-inside.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone:true,
+  selector: 'app-samsung-box-inside',
+  templateUrl: './samsung-box-inside.component.html',
+  styleUrls: ['./samsung-box-inside.component.css']
+})
+export class SamsungBoxInsideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/samsungapp/samsung/samsung.component.ts
+++ src/samsungapp/samsung/samsung.component.ts
@@ -1,16 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { SamsungBoxInsideComponent } from './samsung-box-inside/samsung-box-inside.component';
+import {SamsungProduct} from '../samsung-product';
+import {SAMSUNG} from '../samsung-product-detail';
+
 
 @Component({
   standalone:true,
   selector: 'app-samsung',
   templateUrl: './samsung.component.html',
-  styleUrls: ['./samsung.component.css']
+  styleUrls: ['./samsung.component.css'],
+  imports: [SamsungBoxInsideComponent,CommonModule]
 })
 export class SamsungComponent implements OnInit {
-
+samsung: SamsungProduct []=[];
   constructor() { }
 
   ngOnInit() {
+    this.samsung=SAMSUNG;
   }
 
 }
