a13f7f1849cfded7a4ec05611b7cffd4368e08bf
Jinesh2526 <141109330+Jinesh2526@users.noreply.github.com>
2023-08-13 16:00:45 +0530

    4 files modified, 1 added

--- src/samsungapp/samsung-product-detail.ts
+++ src/samsungapp/samsung-product-detail.ts
@@ -4,9 +4,9 @@ import {SamsungProduct } from "./samsung-product"
 
 
 export const SAMSUNG: SamsungProduct []=[
-{id:1,name:"Galaxy Z Flip5",DP:"https://images.samsung.com/is/image/samsung/assets/in/smartphones/galaxy-z-flip5/buy/ZFlip5_1440x450.jpg?$ORIGIN_JPG$",MRP:109999 ,field:"Calling",info:"	12 MP, f/1.8, 27mm (wide), 1/2.55, 1.4µm, Dual Pixel PDAF, OIS 12 MP, f/2.2, 123˚ (ultrawide), 1.12µm"},
-{id:2,name:"Galaxy Watch6",DP:"https://images.samsung.com/is/image/samsung/p6pim/in/2307/gallery/in-galaxy-watch6-r945-469954-sm-r945fzsains-537406821?$2052_1641_PNG$",MRP:37999,field:"Time",info:"No Cost EMI starts from ₹ 1695.86/ month.A big, circular view on your wrist Develop healthy sleeping habits Get detailed insights on your body and wellness"},
-{id:3,name:"Galaxy Buds2",DP:"https://images.samsung.com/is/image/samsung/p6pim/in/2108/gallery/in-galaxy-buds2-r177-sm-r177nlvainu-481740553?$684_547_PNG$",MRP:6999,field:"Listeing",info:"No Cost EMI starts from ₹ 505.35/ month.Well-balanced Sound Battery Life ANC"},
+{id:1,name:"Galaxy Z Flip5",DP:"https://images.samsung.com/in/smartphones/galaxy-z-flip4/images/galaxy-z-flip4_highlights_kv.jpg",MRP:109999 ,field:"Calling",info:"	12 MP, f/1.8, 27mm (wide), 1/2.55, 1.4µm, Dual Pixel PDAF, OIS 12 MP, f/2.2, 123˚ (ultrawide), 1.12µm"},
+{id:2,name:"Galaxy Watch6",DP:"https://www.sammobile.com/wp-content/uploads/2023/06/leaked-galaxy-watch-6-watch-6-classic.png",MRP:37999,field:"Time",info:"No Cost EMI starts from ₹ 1695.86/ month.A big, circular view on your wrist Develop healthy sleeping habits Get detailed insights on your body and wellness"},
+{id:3,name:"Galaxy Buds2",DP:"https://www.samsung.com/global/galaxy/galaxy-buds2-pro/images/galaxy-buds2-pro-kv-mo.jpg",MRP:6999,field:"Listeing",info:"No Cost EMI starts from ₹ 505.35/ month.Well-balanced Sound Battery Life ANC"},
 
 ]
 
--- src/samsungapp/samsung/samsung-box-inside/samsung-box-inside.component.ts
+++ src/samsungapp/samsung/samsung-box-inside/samsung-box-inside.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { SamsungProduct} from '../../samsung-product';
 @Component({
   standalone:true,
   selector: 'app-samsung-box-inside',
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./samsung-box-inside.component.css']
 })
 export class SamsungBoxInsideComponent implements OnInit {
-
+@Input() samsungProduct!: SamsungProduct;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.samsungProduct);
   }
 
 }
