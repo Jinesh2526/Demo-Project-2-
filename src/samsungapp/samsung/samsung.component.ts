891a84330a43406ef8ad39c2f87ecc5170bd2c55
Jinesh2526 <141109330+Jinesh2526@users.noreply.github.com>
2023-08-14 11:45:08 +0530

    4 files modified

--- src/samsungapp/samsung/samsung.component.ts
+++ src/samsungapp/samsung/samsung.component.ts
@@ -1,23 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { SamsungBoxInsideComponent } from './samsung-box-inside/samsung-box-inside.component';
-import {SamsungProduct} from '../samsung-product';
-import {SAMSUNG} from '../samsung-product-detail';
-
+import { SamsungProduct } from '../samsung-product';
+import { SAMSUNG } from '../samsung-product-detail';
 
 @Component({
-  standalone:true,
+  standalone: true,
   selector: 'app-samsung',
   templateUrl: './samsung.component.html',
   styleUrls: ['./samsung.component.css'],
-  imports: [SamsungBoxInsideComponent,CommonModule]
+  imports: [SamsungBoxInsideComponent, CommonModule],
 })
 export class SamsungComponent implements OnInit {
-samsung: SamsungProduct []=[];
-  constructor() { }
+  samsung: SamsungProduct[] = [];
+  constructor() {}
 
   ngOnInit() {
-    this.samsung=SAMSUNG;
+    this.samsung = SAMSUNG;
   }
-
+  onSelected(SamsungProduct: SamsungProduct) {
+    console.log(SamsungProduct);
+  }
 }
